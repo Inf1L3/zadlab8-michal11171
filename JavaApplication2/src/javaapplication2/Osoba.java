92791ebbf5677650bc7fc8ed7216be3c80853cf1
user <user@LAPTOP-78CH39ND>
2018-10-10 18:30:00 +0200

    Zadania

---
+++ JavaApplication2/src/javaapplication2/Osoba.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author user
+ */
+public abstract class Osoba {
+
+    private final String imie;
+    private final String nazwisko;
+    private final int wiek;
+
+    public Osoba(String imie, String nazwisko, int wiek) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
+
+    abstract public void PokazDane();
+    
+
+    abstract public void Mow();
+
+}
